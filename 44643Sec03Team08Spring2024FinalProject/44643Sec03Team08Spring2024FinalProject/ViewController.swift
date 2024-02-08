6ad7153f6e4e1ec2368ef8b0c86302d54e8d3f59
Harichaithanya kotapati <kotapati.harichaithanya@gmail.com>
2024-02-07 18:20:40 -0600

    Sprint02 readme update

---
+++ 44643Sec03Team08Spring2024FinalProject/44643Sec03Team08Spring2024FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  44643Sec03Team08Spring2024FinalProject
+//
+//  Created by Harchaithanya Kotapati on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
