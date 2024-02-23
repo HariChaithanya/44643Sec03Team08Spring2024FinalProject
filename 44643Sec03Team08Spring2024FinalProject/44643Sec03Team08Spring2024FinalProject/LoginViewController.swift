672de5d351320ff04ba7f170f2c13c141bc80e01
Riyaz Hussian Shaik <shaikriyaz1111@gmail.com>
2024-02-23 08:19:38 -0600

    Riyaz Shaik: Created a new coca touch class for loginView controller and Added required actions and outlets

---
+++ 44643Sec03Team08Spring2024FinalProject/44643Sec03Team08Spring2024FinalProject/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  44643Sec03Team08Spring2024FinalProject
+//
+//  Created by Riyaz Hussian on 2/23/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
