dc03c6443a6a1507c2bccf371e626eb23ee27ce8
UdayaSri61001 <s566578@nwmissouri.edu>
2024-02-23 10:36:20 -0600

    Udaya Sri : Created a new Cocoa Touch Class

---
+++ 44643Sec03Team08Spring2024FinalProject/44643Sec03Team08Spring2024FinalProject/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  44643Sec03Team08Spring2024FinalProject
+//
+//  Created by Udaya Sri Naidu on 2/23/24.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
