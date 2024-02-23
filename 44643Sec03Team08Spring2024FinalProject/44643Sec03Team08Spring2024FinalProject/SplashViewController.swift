e652b267698aaf9aeabfe821961cc0eab01c0907
Harichaithanya kotapati <kotapati.harichaithanya@gmail.com>
2024-02-23 00:02:47 -0600

    Harichaithanya: created a new coco touch ad as a controller for the start up screen and have added some functionalities and added some outlets and functions.

---
+++ 44643Sec03Team08Spring2024FinalProject/44643Sec03Team08Spring2024FinalProject/SplashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SplashViewController.swift
+//  44643Sec03Team08Spring2024FinalProject
+//
+//  Created by Harchaithanya Kotapati on 2/22/24.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        perform(#selector(moveToView), with: nil, afterDelay: 3)
+    }
+    
+    @objc func moveToView() -> Void {
+        
+        //let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+       // self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
