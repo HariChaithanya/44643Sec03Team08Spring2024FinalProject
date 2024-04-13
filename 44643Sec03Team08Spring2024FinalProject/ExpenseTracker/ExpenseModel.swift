0971a7f24d6b763d9c8638c2caa48d15b4c4104f
Riyaz Hussian Shaik <shaikriyaz1111@gmail.com>
2024-04-12 23:28:00 -0500

    Riyaz hussian : create a struct ExpenseModel and have added required attributes

--- 44643Sec03Team08Spring2024FinalProject/ExpenseTracker/ExpenseModel.swift
+++ 44643Sec03Team08Spring2024FinalProject/ExpenseTracker/ExpenseModel.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 struct ExpenseModel: Codable{
-    
+    let id: String
+    let user_id: String
+    let Rent: Double
+    let Groceries: Double
+    let Transportation: Double
+    let Food: Double
+    let Stationery: Double
+    let Healthcare: Double
+    let Entertainment: Double
+    var OTT_Subscrption: Double
+    var Miscellaneous: Double
+    var date: String
     
 }
